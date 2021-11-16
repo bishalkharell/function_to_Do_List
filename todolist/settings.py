43339975a006b017285fddb8f6e06e3b60a53dca
Bishal Kharel <80391504+shadowEyee@users.noreply.github.com>
2021-11-16 19:09:11 +0545

    coded

--- todolist/settings.py
+++ todolist/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')p$$$tf!+((1c9&zqoo-0c6!zi7-_aa3nb76x(2w32ty+orjcy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['todolist-shadow.herokuapp.com','127.0.0.1']
 
